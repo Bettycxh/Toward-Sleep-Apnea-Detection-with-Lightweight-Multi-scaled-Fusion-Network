d451f8e12731de8303282c103de13e2ee2ded254
Bettycxh <44869955+Bettycxh@users.noreply.github.com>
2021-12-20 20:24:34 +0800

    Update SE-MSCNN.py

--- SE-MSCNN.py
+++ SE-MSCNN.py
@@ -1,24 +1,16 @@
 """NOTES: Batch data is different each time in keras, which result in slight differences in results."""
-import time
 import pickle
 import keras
-import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
 import os
-from keras.layers import Conv1D, Dense, Dropout, MaxPooling1D,Reshape, Dense, multiply,  Conv1D,GlobalAveragePooling1D,Dense, Input
-from keras.models import Input, Model
-from keras.models import load_model
+from keras.layers import Dropout, MaxPooling1D,Reshape,multiply,  Conv1D,GlobalAveragePooling1D,Dense
+from keras.models import Input, Model,load_model 
 from keras.regularizers import l2
+from keras.callbacks import ModelCheckpoint, LearningRateScheduler
 from scipy.interpolate import splev, splrep
-from keras.utils import plot_model
-from keras.callbacks import ModelCheckpoint
-from keras.models import Model
-from sklearn.metrics import roc_auc_score
-from keras.callbacks import LearningRateScheduler
-from sklearn.metrics import confusion_matrix
+from sklearn.metrics import confusion_matrix, f1_score
 import random
-from sklearn.metrics import f1_score
 
 base_dir = "./dataset"
 os.environ["CUDA_VISIBLE_DEVICES"]="0,1"
@@ -42,9 +34,9 @@ def load_data(path):
 		# Curve interpolation
         rri_interp_signal = splev(tm, splrep(rri_tm, scaler(rri_signal), k=3), ext=1)
         ampl_interp_signal = splev(tm, splrep(ampl_tm, scaler(ampl_siganl), k=3), ext=1)
-        x_train1.append([rri_interp_signal, ampl_interp_signal])
-        x_train2.append([rri_interp_signal[180:720], ampl_interp_signal[180:720]])
-        x_train3.append([rri_interp_signal[360:540], ampl_interp_signal[360:540]])
+        x_train1.append([rri_interp_signal, ampl_interp_signal])#5-minute-long segment
+        x_train2.append([rri_interp_signal[180:720], ampl_interp_signal[180:720]])#3-minute-long segment
+        x_train3.append([rri_interp_signal[360:540], ampl_interp_signal[360:540]])#1-minute-long segment
     x_training1 = []
     x_training2 = []
     x_training3 = []
@@ -149,19 +141,18 @@ def create_model(input_a_shape,input_b_shape,input_c_shape, weight=1e-3):
     model = Model(inputs=[input1,input2,input3], outputs=outputs)
     return model
 if __name__ == "__main__":
+	#load_data
     path="apnea-ecg.pkl"
     x_train1,x_train2,x_train3, y_train, groups_train,x_val1,x_val2,x_val3, y_val, groups_val, x_test1, x_test2, x_test3, y_test, groups_test = load_data(path)
     y_train = keras.utils.to_categorical(y_train, num_classes=2) # Convert to two categories
     y_val = keras.utils.to_categorical(y_val, num_classes=2)
     y_test = keras.utils.to_categorical(y_test, num_classes=2)
     print('input_shape',x_train1.shape,x_train2.shape,x_train3.shape)
-
+	
+	#training
     model = create_model(x_train1.shape[1:],x_train2.shape[1:],x_train3.shape[1:])
-    # compile
     model.compile(loss='binary_crossentropy', optimizer='adam', metrics=['accuracy'])
-
     filepath='weights.best.hdf5'
-    # checkpoint
     checkpoint = ModelCheckpoint(filepath, monitor='val_accuracy', verbose=1, save_best_only=True,mode='max')
     lr_scheduler = LearningRateScheduler(lr_schedule)
     callbacks_list = [lr_scheduler, checkpoint]
@@ -169,23 +160,20 @@ if __name__ == "__main__":
                         validation_data=([x_val1, x_val2, x_val3], y_val),callbacks=callbacks_list)
 
     # test the model
+	path="apnea-ecg.pkl"
+    x_train1,x_train2,x_train3, y_train, groups_train,x_val1,x_val2,x_val3, y_val, groups_val, x_test1, x_test2, x_test3, y_test, groups_test = load_data(path)
+    filepath='./weights.best.hdf5'
+    model = load_model(filepath)
+    print("testing:")
     loss, accuracy = model.evaluate([x_test1, x_test2, x_test3], y_test)
     # save prediction score
     y_score = model.predict([x_test1, x_test2, x_test3])
     roc=roc_auc_score(y_score=y_score,y_true=y_test)
     output = pd.DataFrame({"y_true": y_test[:, 1], "y_score": y_score[:, 1], "subject": groups_test})
     output.to_csv("./utils/code_for_calculating_per-recording/output/SEMSCNN.csv", index=False)
-
-
-    path="apnea-ecg.pkl"
-    x_train1,x_train2,x_train3, y_train, groups_train,x_val1,x_val2,x_val3, y_val, groups_val, x_test1, x_test2, x_test3, y_test, groups_test = load_data(path)
-    filepath='./weights.best.hdf5'
-    model = load_model(filepath)
-    print("testing:")
     y_true, y_pred = y_test, np.argmax(model.predict([x_test1, x_test2, x_test3], batch_size=1024, verbose=1), axis=-1)
     C = confusion_matrix(y_true, y_pred, labels=(1, 0))
     TP, TN, FP, FN = C[0, 0], C[1, 1], C[1, 0], C[0, 1]
     acc, sn, sp = 1. * (TP + TN) / (TP + TN + FP + FN), 1. * TP / (TP + FN), 1. * TN / (TN + FP)
     f1=f1_score(y_true, y_pred, average='binary')
-
     print("acc: {}, sn: {}, sp: {}, f1: {}".format(acc, sn, sp, f1))
